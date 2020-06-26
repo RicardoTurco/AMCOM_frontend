002aa16b99bf014bf31c95538429bc4ac76664a2
RicardoTurco <ricturco@gmail.com>
2020-06-26 16:41:59 -0300

    Configuring for deploy on Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn amcom.wsgi --log-file -
--- amcom/settings.py
+++ amcom/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+_!o_!cv^2xbvsgqo@1(ivm%l73uc54c0lhl%8=b@(&j@4)bc7'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['movbancariasamcom.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
@@ -74,12 +76,8 @@ WSGI_APPLICATION = 'amcom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl())}
 
 
 # Password validation
